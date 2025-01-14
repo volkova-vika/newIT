23867738f5d87ce921ad08aae3950e2aa963c306
volkova-vika <viktoriavoukava@Viktorias-MacBook-Air.local>
2025-01-14 20:45:22 +0100

    js

---
+++ lesson11/index.js
@@ -0,0 +1,51 @@
+// // confirm("Please enter");
+// const nickName = prompt("Enter your name","vika");
+// console.log(`hello, ${nickName}`);
+
+const str = 'SOME random StriNG';
+// const str2 = "next string";
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// console.log(str);
+
+// const strUp = str.toUpperCase();
+// console.log(strUp);
+
+// console.log(str.concat(str2, "fkjeufb", 234, true));
+// console.log(1 + "1");
+// console.log("50" + 25);
+// console.log("50" - 25);
+
+// console.log(str.repeat(5));
+
+// console.log(str.toLowerCase().includes('str'));
+// const strLow = str.toLowerCase();
+// const isHas = str.toLowerCase.includes("str");
+// console.log(isHas);
+
+// console.log(str.startsWith("SOME"));
+// console.log(str.endsWith("string"));
+
+// console.log(str.replaceAll("S", "$").replaceAll("a", "@"));
+// console.log(str.replace(/s/gi, "$"));
+// console.log(str.charAt(6));
+// console.log(str.indexOf("random"));
+// console.log(str.lastIndexOf("S"));
+
+console.log(str.slice(5,11));
+console.log(str.substring(5, 11));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson12/index.js
@@ -0,0 +1,55 @@
+// const arr = [1, 'two', null, true, { s:2 }, [1, 2, 3]];
+// // console.log(arr[4]);
+// console.log(arr[5][1]);
+
+const arr = [0,1,2,3,4];
+const arr2 = [5,6,7,8,9];
+// console.log(arr.concat(arr2,"LOL"));
+// arr.push('five', 'six', 'seven', 'eight')
+// console.log(arr);
+// console.log(ele);
+
+// arr.unshift('five', 'six', 'seven');
+// console.log(arr);
+
+// const data = arr.pop();
+// console.log(arr);
+// console.log(data);
+
+// const data = arr.shift();
+// console.log(arr);
+// console.log(data);
+
+
+// arr.reverse();
+// console.log(arr);
+
+// const revArr = arr.toReversed();
+// console.log(arr);
+// console.log(revArr);
+// console.log(arr);
+
+// console.log(arr.slice(1,4));
+// const arrSplice = arr.splice(1,3);
+
+const age = 18;
+
+if (age > 18) {
+    console.log('> 18');
+} else {
+    console.log('< 18');
+}
+>=
+<=
+===
+
+
+
+
+
+
+
+
+
+
+
