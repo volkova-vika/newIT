de1f8625bf861d82d6677963adb9ddd642acb7f6
volkova-vika <viktoriavoukava@Viktorias-MacBook-Air.local>
2025-01-21 14:27:55 +0100

    unfinished_home13

---
+++ lesson13/index2.js
@@ -0,0 +1,32 @@
+// let arr = [1,3,5,3,6,7,8,8,8,4,2,7,8,7,9,5,6,6,3,9,4,7,3,3,4,5,76,7,8,9,5,6,4,4];
+// let unique = [];
+// let dupli;
+// let in = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     dupli = false;
+// }
+
+// for (let j = 0; j < unique.length; j++) {
+//     if (arr[i] === unique[j]) {
+//         dupli = true;
+//         break;
+//     }
+// }
+// console.log(arr);
+
+// let num = 5;
+// let factorial = 1;
+// for (let i = 1; i <= num; i++) {
+//     factorial *= i; 
+// }
+// console.log(factorial);
+
+
+// let year = 2025; 
+
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//     console.log(year + ' високосный');
+// } else {
+//     console.log(year + ' не високосный');
+// }
