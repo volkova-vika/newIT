ae925c9607bd62d910e3be4791dd37b043546fe8
volkova-vika <viktoriavoukava@Viktorias-MacBook-Air.local>
2025-01-24 15:30:24 +0100

    home13update

--- lesson13/index2.js
+++ lesson13/index2.js
@@ -1,21 +1,13 @@
-// let arr = [1,3,5,3,6,7,8,8,8,4,2,7,8,7,9,5,6,6,3,9,4,7,3,3,4,5,76,7,8,9,5,6,4,4];
-// let unique = [];
-// let dupli;
-// let in = 0;
-
+// const arr = [1,3,5,3,6,7,8,8,8,4,2,7,8,7,9,5,6,6,3,9,4,7,3,3,4,5,76,7,8,9,5,6,4,4];
+// let uniqie = []
 // for (let i = 0; i < arr.length; i++) {
-//     dupli = false;
-// }
-
-// for (let j = 0; j < unique.length; j++) {
-//     if (arr[i] === unique[j]) {
-//         dupli = true;
-//         break;
+//     if(!uniqie.includes(arr[i])) {
+//         uniqie.push(arr[i]);
 //     }
 // }
-// console.log(arr);
+// console.log('uniqie values', uniqie);
 
-// let num = 5;
+// let num = 7;
 // let factorial = 1;
 // for (let i = 1; i <= num; i++) {
 //     factorial *= i; 
@@ -30,3 +22,28 @@
 // } else {
 //     console.log(year + ' не високосный');
 // }
+
+// const arr = [46,4,45,67,446,8856,356,8854,5,8,4,67,446,8856,46,4,45,67,446];
+// let countEl = [];
+//  for (let i = 0; i < arr.length; i++) {
+//     let check = arr[i];
+//     countEl[check] = (countEl[check] || 0) +1; 
+//  }
+//  console.log("how many elements in array",countEl);
+
+const phone = {
+    brand: "iPhone",
+    model: 16,
+    type: "Pro",
+    color: "black",
+    comment: "Made in Silicon Valley",
+    techSet: {
+        capacity: 128,
+        display: "6,7",
+        chip: "A18 chip"
+    }
+};
+console.log("phone", phone);
+
+
+ 
